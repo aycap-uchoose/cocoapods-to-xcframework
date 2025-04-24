ba7ba882b9c6a05edcbd1a821adf7ab210c82803
prongbang <prongbang@gmail.com>
2025-04-24 15:09:44 +0700

    Initial Commit

---
+++ material-components-ios-spm/material_components_ios_spmApp.swift
@@ -0,0 +1,17 @@
+//
+//  material_components_ios_spmApp.swift
+//  material-components-ios-spm
+//
+//  Created by Songsit Pantayang on 24/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct material_components_ios_spmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
