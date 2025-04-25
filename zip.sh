6cf2299fbece9d65832d4ace6d55c643272c2181
prongbang <prongbang@gmail.com>
2025-04-25 11:36:17 +0700

    refactor: move checksum to shel script

--- zip.sh
+++ zip.sh
@@ -1,6 +1,3 @@
 FRAMEWORK=$1
 
 zip -r ./Framework/Zip/${FRAMEWORK}.xcframework.zip ./Framework/${FRAMEWORK}.xcframework
-
-echo "${FRAMEWORK} Checksum:"
-swift package compute-checksum ./Framework/Zip/${FRAMEWORK}.xcframework.zip
